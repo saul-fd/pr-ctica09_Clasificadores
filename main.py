6364783daeb9b0032bbdf3b7b1d12ede5ca95c9d
saul-fd <132414518+saul-fd@users.noreply.github.com>
2026-05-22 07:18:55 -0600

    Fix HTML formatting in markdown for clarity

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ if seccion == "Resumen general":
         st.markdown("""<div class='card' style='border-left:3px solid #f4a94e;'>
             <b>Desbalance: 83.9% vs 16.1%</b><br>
             <span style='color:#5a8ab0;font-size:13px;'>La mayoría de modelos aprenden a predecir
-            "No renuncia" casi siempre, inflando el Accuracy pero perjudicando el Recall.</span>
+            </span>
         </div>""", unsafe_allow_html=True)
 
     with col2:
